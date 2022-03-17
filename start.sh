08650869116bbba879be47826f79217fc5fc29cf
wodlxosxos <wodlxosxos73@gmail.com>
2022-03-17 16:21:21 +0900

    for manifest

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+kubectl apply -f ingress.yaml
+
+minikube addons enable ingress
+minikube addons enable metrics-server
+
+kubectl apply -f hpa.yaml
+kubectl apply -f load.yaml
